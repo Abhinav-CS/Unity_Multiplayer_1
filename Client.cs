e2f4b967cc5e9a5088be90f0e4bb0e6dc978cfc0
Abhinav-CS <83754268+Abhinav-CS@users.noreply.github.com>
2021-05-06 13:48:46 +0530

    Add files via upload

--- Client.cs
+++ Client.cs
@@ -1,72 +1,81 @@
-﻿using System;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
-namespace FPS_GAME_SERVER
+using System;
+
+public class Client : MonoBehaviour
 {
-    class Client
+    public static Client instance;
+    public static int dataBufferSize = 4096;
+
+    public string ip = "127.0.0.1";
+    public int port = 26950;
+    public int myId = 0;
+    public TCP tcp;
+
+    private void Awake()
     {
-        public static int dataBufferSize = 4096; 
-        public int id;
-        public TCP tcp;
-
-        public Client(int _clientId)
+        if (instance == null)
         {
-            id = _clientId;
-            tcp = new TCP(id);
+            instance = this;
         }
-         
-        public class TCP
+        else if (instance != this)
         {
-            public TcpClient socket;
-            private readonly int id;
-            private NetworkStream stream;
-            private byte[] receiveBuffer;
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
 
+    private void Start()
+    {
+        tcp = new TCP();
+    }
+    public void ConnectToServer()
+    {
+        tcp.Connect();
+    }
+    public class TCP
+    {
+        public TcpClient socket;
 
-            public TCP(int _id)
+        private NetworkStream stream;
+        private byte[] receiveBuffer;
+        public void Connect()
+        {
+            socket = new TcpClient
             {
-                id = _id;
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
+
+        }
+        private void ConnectCallback(IAsyncResult _result)
+        {
+            socket.EndConnect(_result);
+
+            if(!socket.Connected)
+            {
+                return;
             }
-            public void Connect(TcpClient _socket)
+
+            stream = socket.GetStream();
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }       
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            try
             {
-                socket = _socket;
-                socket.ReceiveBufferSize = dataBufferSize;
-                socket.SendBufferSize = dataBufferSize;
 
-                stream = socket.GetStream();
-
-                receiveBuffer = new byte[dataBufferSize];
-
-                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
-
-                // TO DO:  SEND WELCOME PACKET.
             }
-        
-            private void ReceiveCallback(IAsyncResult _result)
+            catch
             {
-                try
-                {
-
-                    int _byteLength = stream.EndRead(_result);
-                    if(_byteLength <= 0)
-                    {
-                        // to do: disconnect
-                        return;
-                    }
-                    byte[] _data = new byte[_byteLength];
-                    Array.Copy(receiveBuffer, _data, _byteLength);
-                    // TO DO: handle 
-
-                    stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
-                }  
-                catch(Exception _ex)
-                {
-                    Console.WriteLine($"Error receiving TCP data: {_ex}");
-                    //TO DO: disconnect
-                }
-                
+                //TO DO: dissconnect
             }
         }
 
