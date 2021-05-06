51591b87a956465ca8295d1d2bdd344931d92f61
Abhinav-CS <83754268+Abhinav-CS@users.noreply.github.com>
2021-05-06 13:46:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FPS_GAME_SERVER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "FPS GAME SERVER";
+
+            Server.Start(50, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
